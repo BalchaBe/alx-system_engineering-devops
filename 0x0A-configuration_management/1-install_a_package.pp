19e868a13a85d93a86ab272ac71f3fa2b1ac31b1
BalchaBe <balcha36@yahoo.com>
2024-01-19 03:37:57 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
-# Install an especific version of flask (3.7.0)
+# Install an especific version of flask (2.1.0)
 package {'flask':
-  ensure   => '3.7.0',
+  ensure   => '2.1.0',
   provider => 'pip3'
 }
